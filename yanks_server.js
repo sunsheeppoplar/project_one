3d6cf12465f82fe11a401d806d8d454ec35bb94d
Abner Yang <abtyang@gmail.com>
2015-04-19 20:40:54 -0400

    create document functionality

--- yanks_server.js
+++ yanks_server.js
@@ -67,30 +67,12 @@ app.post('/documents', function(req, res) {
 	db.run("INSERT INTO documents (title, body, image) VALUES (?, ?, ?)", titleInput, bodyInput, imageInput, function(err) {
 		if (err) {
 			console.log(err)
-		// *** Have to figure out a way to sanitize this ***
-		// } else {
-		// 	var lastInsertRowId = 'last_insert_rowid()'
-		// 	var selectStatement = 'SELECT id FROM authors WHERE name =' + " '" + userInput + "'"
-		// 	db.run("INSERT INTO authorships (auth_id, doc_id) VALUES (?, ?)", selectStatement.toString(), lastInsertRowId.toString(), function(err) {
-		// 		if (err) {
-		// 			console.log(err)
-		// 		} else {
-		// 			res.redirect('/documents')
-		// 		}
-		// 	});
-		// }
 		} else {
-			db.run("INSERT INTO authorships (doc_id) VALUES (last_insert_rowid()", function(err) {
+			db.run("INSERT INTO authorships (auth_name, doc_title) VALUES (?, ?)", userInput, titleInput, function(err) {
 				if (err) {
 					console.log(err)
-				} else { 
-					db.all("INSERT INTO authorships (auth_id) SELECT id FROM authors WHERE name = ?", userInput, function(err) {
-						if (err) {
-							console.log(err)
-						} else {
-							res.redirect('/documents')
-						}
-					});
+				} else {
+					res.redirect('/documents')
 				}
 			});
 		}
